92148a274937780e7cde541638cae717dd323ad7
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-06-24 03:43:15 +0000

    linked list #2 complete

---
+++ linked_list_2.rb
@@ -0,0 +1,92 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class Stack
+  attr_reader :data
+
+  def initialize
+      @data = nil
+  end
+
+  # Push a value onto the stack
+  def push(value)
+    new_node = LinkedListNode.new(value, nil)
+    if @data == nil
+      @data = new_node
+      return
+    end
+    temp_node = @data
+    
+    while @data.next_node
+      @data = @data.next_node
+    end
+    @data.next_node = new_node
+    @data = temp_node
+  end
+
+  # Pop an item off the stack.
+  # Remove the last item that was pushed onto the
+  # stack and return the value to the user
+  def pop
+    list_node = @data
+    if list_node == nil
+      return nil
+    end
+    while @data.next_node
+      if @data.next_node.next_node == nil
+        val = @data.next_node.value
+        @data.next_node = nil
+        @data = list_node
+        return val
+      end
+      @data = @data.next_node
+    end
+    val = @data.value
+    @data = nil
+    return val
+  end
+
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list, previous=nil)
+  while list
+    temp_node = list
+    list = list.next_node
+    temp_node.next_node = previous
+    previous = temp_node
+  end
+  return temp_node
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+node4 = LinkedListNode.new(17, node3)
+node5 = LinkedListNode.new(102, node4)
+
+print_values(node5)
+
+puts "----------------------------------------------------------"
+
+revlist = reverse_list(node5, nil)
+
+print_values(revlist)
+
+
+
